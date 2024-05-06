0825eff10603f66be1dbdf3d3dd42c11c5cbb067
YelloElefant <alextrotternz@gmail.com>
2024-05-06 01:12:57 +0000

    adding download.js download.js downloads a file from a url

---
+++ src/download.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const client = require('https');
+
+function downloadImage(url, filepath) {
+    return new Promise((resolve, reject) => {
+        client.get(url, (res) => {
+            if (res.statusCode === 200) {
+                res.pipe(fs.createWriteStream(filepath))
+                    .on('error', reject)
+                    .once('close', () => resolve(filepath));
+            } else {
+                // Consume response data to free up memory
+                res.resume();
+                reject(new Error(`Request Failed With a Status Code: ${res.statusCode}`));
+
+            }
+        });
+    });
+}
+
+downloadImage('', '../images/lena.png')
+    .then(console.log)
+    .catch(console.error);
