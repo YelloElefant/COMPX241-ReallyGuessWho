c1092b03f2419ffce5a8bacc5ea10d35caa0b37f
YelloElefant <alextrotternz@gmail.com>
2024-05-16 03:09:03 +0000

    lobby displays all current matches
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- src/Lobby.js
+++ src/Lobby.js
@@ -2,6 +2,49 @@ import { LobbyClient } from 'boardgame.io/client';
 
 const lobbyClient = new LobbyClient({ server: 'http://localhost:8081' });
 
-lobbyClient.listGames()
-   .then(console.log) // => ['chess', 'tic-tac-toe']
-   .catch(console.error);
+
+async function matches() {
+
+   lobbyClient.listGames()
+      .then(console.log) // => ['chess', 'tic-tac-toe']
+      .catch(console.error);
+
+
+   const gameId = 'guesswho';
+
+   const { matchID } = await lobbyClient.createMatch('guesswho', {
+      numPlayers: 2
+   });
+   console.log("matchID: ", matchID); // => '123'
+
+
+   const { matches } = await lobbyClient.listMatches('guesswho');
+   console.log("matches: ", matches); // => [{ matchID: '123', players: ['0', '1'] }, ...]
+   const playerId = await prompt("Player ID: ");
+   const name = await prompt("Name: ");
+
+
+   const { playerCredentials } = await lobbyClient.joinMatch(
+      gameId,
+      matchID,
+      {
+         playerID: playerId,
+         playerName: name,
+      }
+   );
+
+   console.log("playerCredentials: ", playerCredentials); // => '123-456'
+
+   const gameList = document.getElementById('gameList');
+
+   for (const match of matches) {
+      const game = document.createElement('li');
+      game.appendChild(document.createTextNode(`Match ID: ${match.matchID} - Player ID: ${playerId} - Game: ${match.gameName}`));
+      gameList.appendChild(game);
+   }
+
+}
+
+matches();
+
+
