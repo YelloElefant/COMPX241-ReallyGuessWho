31abb849d2623815ce959137e540059c11c90d7d
YelloElefant <alextrotternz@gmail.com>
2024-06-03 11:33:49 +0000

    updating cors
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- src/server.js
+++ src/server.js
@@ -2,11 +2,6 @@ const { Server, Origins } = require('boardgame.io/server');
 const { GuessWho } = require('./Game');
 
 
-
-
-
-
-
 const server = Server({
    games: [GuessWho],
    origins: [
@@ -17,13 +12,25 @@ const server = Server({
       'https://guesswho.yelloelefant.com',
       'https://guesswhoserver.yelloelefant.com',
       'https://guesswholobby.yelloelefant.com',
-      'http://localhost'
+      'http://localhost',
+      'http://192.168.1.29'
    ],
 });
 
 const lobbyConfig = {
    apiPort: 8081,
    apiCallback: () => console.log('Running Lobby API on port 8081...'),
+   origins: [
+      'https://yelloelefant.com',
+      Origins.LOCALHOST,
+      Origins.SAME_IP,
+      Origins.LOCALHOST_IN_DEVELOPMENT,
+      'https://guesswho.yelloelefant.com',
+      'https://guesswhoserver.yelloelefant.com',
+      'https://guesswholobby.yelloelefant.com',
+      'http://localhost',
+      'http://192.168.1.29'
+   ]
 };
 
 server.run({ port: 8000, lobbyConfig });
